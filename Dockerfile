fd0e0df5d033131b5efad1be2b9f144ac51b1986
Fakhreza SUparyadi <fakhreza.spy@gmail.com>
2023-11-07 10:48:16 +0700

    [Feature] Adding dockerfile and docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:18-alpine
 WORKDIR /app
 COPY . .
-RUN yarn install --production
+RUN yarn install && yarn start
 CMD ["node", "src/index.js"]
 ENV PORT=10000
 EXPOSE 10000
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,6 @@ class Controller{
             username:req.body.username,
             password:req.body.password
         }
-        console.log(req.body)
         userData.password = encode(userData.password)
         User
         .create(userData)
