02b99ff9e7f7e6db2f1f554ab01d383023abb666
Fakhreza Suparyadi <fakhreza.dwiputra@dansmultipro.com>
2024-08-11 11:16:35 +0700

    gefami

--- HowToRun.md
+++ HowToRun.md
@@ -1,5 +1,6 @@
 ## How To RUn
 
+USE NODE 14.17.3
 1. git clone
 2. npm i
 3. Setup dan ubah config database di config.js
@@ -8,3 +9,4 @@
 6. npx sequelize-cli db:seed:all
 7. npm run dev to run dev server
 8. no need .env
+9. import postman collection in folder collection
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const userController = require("../controllers/userController");
 
 router.post("/register", userController.registerUser);
-router.get("/login", userController.loginUser);
+router.post("/login", userController.loginUser);
 
 module.exports = router;
